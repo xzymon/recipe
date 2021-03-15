d95eaff011ea103bfe68a0ce158e7bbb05d5802a
Szymon Ignaciuk <xzymon@gmail.com>
2021-03-15 19:30:18 +0100

    Some JPA Entities: Recipe, Notes, Ingredient, UnitOfMeasure

---
+++ src/main/java/com/xzymon/recipe/domain/Ingredient.java
@@ -0,0 +1,51 @@
+package com.xzymon.recipe.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String description;
+	private BigDecimal amount;
+
+	@OneToOne
+	private UnitOfMeasure uom;
+
+	@ManyToOne
+	private Recipe recipe;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public UnitOfMeasure getUom() {
+		return uom;
+	}
+
+	public void setUom(UnitOfMeasure uom) {
+		this.uom = uom;
+	}
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+}
